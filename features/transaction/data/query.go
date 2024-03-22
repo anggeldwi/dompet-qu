501260a3d57fcbb485776017ab4aae5af10c21bd
anggeldwi <dwimirandaa@gmail.com>
2024-03-23 06:51:20 +0700

    first commit

--- features/transaction/data/query.go
+++ features/transaction/data/query.go
@@ -20,64 +20,6 @@ func New(db *gorm.DB, mi externalapi.MidtransInterface) transaction.TransactionD
 	}
 }
 
-// // InsertTransaction implements transaction.TransactionDataInterface.
-// func (repo *transactionQuery) TopUp(userIdLogin int, inputTrasaction transaction.Core) (*transaction.Core, error) {
-// 	// var totalHargaKeseluruhan int
-// 	// var packageGorm pd.Package
-// 	// ts := repo.db.Where("tour_id = ? AND id = ?", inputTrasaction.TourID, inputBooking.PackageID).First(&packageGorm)
-// 	// if ts.Error != nil {
-// 	// 	return nil, ts.Error
-// 	// }
-
-// 	// if inputBooking.VoucherID != nil {
-// 	// 	var voucherGorm vd.Voucher
-// 	// 	ts := repo.db.Where("id = ?", inputBooking.VoucherID).First(&voucherGorm)
-// 	// 	if ts.Error != nil {
-// 	// 		return nil, ts.Error
-// 	// 	}
-
-// 	// 	var existingUseVoucher Booking
-// 	// 	if err := repo.db.Where("user_id = ? AND voucher_id = ?", userIdLogin, inputBooking.VoucherID).First(&existingUseVoucher).Error; err == nil {
-// 	// 		return nil, errors.New("user has already used this voucher")
-// 	// 	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-// 	// 		return nil, err
-// 	// 	}
-
-// 	// 	totalHargaAwal := packageGorm.Price * inputBooking.Quantity
-// 	// 	if totalHargaAwal < voucherGorm.DiscountValue {
-// 	// 		return nil, errors.New("maaf, anda tidak bisa menggunakan voucher ini karena total pembayaran anda terlalu rendah")
-// 	// 	} else {
-// 	// 		// totalHargaKeseluruhan = ((packageGorm.JumlahTiket * packageGorm.Price) * inputBooking.Quantity) - voucherGorm.DiscountValue
-// 	// 		totalHargaKeseluruhan = totalHargaAwal - voucherGorm.DiscountValue
-// 	// 	}
-// 	// } else {
-// 	// 	// totalHargaKeseluruhan = (packageGorm.JumlahTiket * packageGorm.Price) * inputBooking.Quantity
-// 	// 	totalHargaKeseluruhan = packageGorm.Price * inputBooking.Quantity
-// 	// }
-
-// 	// inputTrasaction.Amount = inputTrasaction.Amount
-
-// 	payment, errPay := repo.paymentMidtrans.NewTransaction(inputTrasaction)
-// 	if errPay != nil {
-// 		return nil, errPay
-// 	}
-
-// 	transactionModel := CoreToModelTransction(inputTrasaction)
-// 	transactionModel.PaymentType = payment.PaymentType
-// 	transactionModel.Status = payment.Status
-// 	transactionModel.VaNumber = payment.VaNumber
-// 	transactionModel.ExpiredAt = payment.ExpiredAt
-
-// 	tx := repo.db.Create(&transactionModel)
-// 	if tx.Error != nil {
-// 		return nil, tx.Error
-// 	}
-
-// 	transactionCore := ModelToCoreTransaction(transactionModel)
-
-// 	return &transactionCore, nil
-// }
-
 func (repo *transactionQuery) TopUp(userIdLogin int, inputTransaction transaction.Core) (*transaction.Core, error) {
 	// Membuat transaksi pembayaran
 	payment, errPay := repo.paymentMidtrans.NewTransaction(inputTransaction)
